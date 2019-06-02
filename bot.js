85c925d18e8739b42f28d3bf3039628fc39959f7
R3dAlArb <45834562+R3dAlArb@users.noreply.github.com>
2019-06-02 23:13:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("2play TheEmpire",{type: 'WATCHING'})
+   client.user.setActivity("2play Runes",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
